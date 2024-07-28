74a25fbba1ef148df0b7d0eee4e41edf08b13444
Jeffrey King <jking@hey.com>
2024-07-28 19:41:33 +0800

    add fixes and cleanup to header issues

--- scripts/city/mexico_city.py
+++ scripts/city/mexico_city.py
@@ -7,7 +7,6 @@ from playwright.sync_api import sync_playwright
 project_root = os.getenv('PROJECT_ROOT')
 sys.path.insert(0, project_root)
 import scripts.utils as utils
-import scripts.utils_playwright as utils_playwright
 
 # FILE_NAME = "mexico_city_all_trips.csv"
 CSV_PATH = utils.get_raw_files_directory("mexico_city")
@@ -43,15 +42,11 @@ def run_get_exports(playwright, url, csv_path):
             download = download_info.value
             download.save_as(os.path.join(csv_path, download.suggested_filename))
 
-
-    # Close the browser
     browser.close()
 
 def get_exports(url, csv_path):
     with sync_playwright() as playwright:
         run_get_exports(playwright, url, csv_path)
-
-
 
 mexico_city_renamed_columns = {
     "Genero_Usuario": "gender", 
@@ -95,20 +90,24 @@ def create_df_with_all_trips(folder_path):
 
     for file_path in csv_files:
         print(file_path)
-        df = pl.read_csv(file_path, schema_overrides={ 
+        df = pl.read_csv(file_path, schema_overrides={
+            # 2018-12.csv - `C037` as dtype `i64` at column 'Bici'
             "Bici": pl.String, 
+            # 2024-05.csv - `273-274` as dtype `i64` at column 'Ciclo_Estacion_Retiro' 
             "Ciclo_Estacion_Retiro": pl.String, 
+            # 2019-03.csv - `32:11.8` as dtype `i64` at column 'Ciclo_Estacion_Arribo'
             "Ciclo_Estacion_Arribo": pl.String, 
-            # Typo error in column header
+            # 2022-08.csv - Typo error in column header - `390-391` as dtype `i64` at column 'Ciclo_EstacionArribo'
             "Ciclo_EstacionArribo": pl.String,
-            # Problem csv - 2022-09.csv
+            # # Problem csv - 2022-09.csv - `390-391` as dtype `i64` at column 'CE_retiro' 
             "CE_retiro": pl.String,
             "CE_arribo": pl.String
-        }, null_values="NULL")
-        
+        }, 
+            # 2022_10.csv - `NULL` as dtype `i64` at column 'Edad_Usuario'
+            null_values="NULL")
         headers = df.columns
-        print(headers)
-                
+        
+        ### Single header errors in csvs
         if "Ciclo_EstacionArribo" in headers:
             df = df.rename({"Ciclo_EstacionArribo": "Ciclo_Estacion_Arribo"})
         if 'Fecha Arribo' in headers:
@@ -120,30 +119,28 @@ def create_df_with_all_trips(folder_path):
         if "Hora_Retiro_duplicated_0" in headers:
             df = df.rename({ "Hora_Retiro_duplicated_0": "Hora_Arribo"})
 
-
-        if "2020-10.csv" in file_path or "2020-05.csv" in file_path or "2020-11.csv" in file_path or "2020-07.csv" in file_path:
-            print("skipped 2020-10.csv")
-        else:
-            renamed_columns = mexico_city_renamed_columns
-            if "Genero_usuario" in headers:
-                renamed_columns = mexico_city_misaligned_renamed_columns
-                
-            df = df.rename(renamed_columns).with_columns([
-                pl.coalesce([pl.col("start_date").str.strptime(pl.Datetime, format, strict=False) for format in date_formats]),
-                pl.coalesce([pl.col("end_date").str.strptime(pl.Datetime, format, strict=False) for format in date_formats]),
-                pl.coalesce([pl.col("starting_time").str.replace(r"\.\d+", "").str.zfill(8).str.strptime(pl.Time, format, strict=False) for format in time_formats]),
-                pl.coalesce([pl.col("ending_time").str.replace(r"\.\d+", "").str.zfill(8).str.strptime(pl.Time, format, strict=False) for format in time_formats]),
-            ]).with_columns([
-                pl.col("start_date").dt.combine(pl.col("starting_time")).alias("start_time"),
-                pl.col("end_date").dt.combine(pl.col("ending_time")).alias("end_time")
-            ])
-            df = df.select(["start_time", "end_time", "start_date", "end_date", "starting_time", "ending_time"])
-            dataframes.append(df)
+        renamed_columns = mexico_city_renamed_columns
+        if "Genero_usuario" in headers:
+            renamed_columns = mexico_city_misaligned_renamed_columns
+        
+        df = df.rename(renamed_columns).with_columns([
+            pl.coalesce([pl.col("start_date").str.strptime(pl.Datetime, format, strict=False) for format in date_formats]),
+            pl.coalesce([pl.col("end_date").str.strptime(pl.Datetime, format, strict=False) for format in date_formats]),
+            pl.coalesce([pl.col("starting_time").str.replace(r"\.\d+", "").str.zfill(8).str.strptime(pl.Time, format, strict=False) for format in time_formats]),
+            pl.coalesce([pl.col("ending_time").str.replace(r"\.\d+", "").str.zfill(8).str.strptime(pl.Time, format, strict=False) for format in time_formats]),
+        ]).with_columns([
+            pl.col("start_date").dt.combine(pl.col("starting_time")).alias("start_time"),
+            pl.col("end_date").dt.combine(pl.col("ending_time")).alias("end_time")
+        ])
+        df = df.select(["start_time", "end_time", "start_date", "end_date", "starting_time", "ending_time"])
+        dataframes.append(df)
 
     combined_df = pl.concat(dataframes)
     return combined_df
 
 def build_trips(args):
+    if not args.skip_unzip:
+        get_exports(MEXICO_CITY_OPEN_DATA_URL, CSV_PATH)
     df = create_df_with_all_trips(MEXICO_CSVS_PATH)
     utils.create_all_trips_file(df, args)
     utils.log_final_results(df, args)
