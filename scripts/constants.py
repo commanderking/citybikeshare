7aa982f5257ec7b217f9e6a7b916866e23a5b18d
Jeffrey King <jking@hey.com>
2025-10-18 19:57:28 -0700

    use old way of handling vancouver

--- scripts/constants.py
+++ scripts/constants.py
@@ -567,24 +567,26 @@ config = {
             "convert_to_datetime",
         ],
     },
-    "vancouver": {
-        "name": "vancouver",
-        "system_name": "vancouver_bikeshare",
-        "file_matcher": ["Mobi_System_Data"],
-        "renamed_columns": vancouver_renamed_columns,
-        "date_formats": ["%Y-%m-%d %H:%M", "%m/%d/%Y %H:%M"],
-        "read_csv_options": {
-            "encoding": "utf8-lossy",
-            "dtypes": {"Covered distance (m)": pl.Float64},
-        },
-        "processing_pipeline": {
-            "filter_null_rows",
-            "rename_columns",
-            "randomize_datetimes",
-            "offset_two_digit_years",
-            "select_final_columns",
-        },
-    },
+    # Vancouver not working
+    # For some reason, 2020-03 cannot be read properly when using this as csv, but when using vancouver's custom function it works
+    # "vancouver": {
+    #     "name": "vancouver",
+    #     "system_name": "vancouver_bikeshare",
+    #     "file_matcher": ["Mobi_System_Data"],
+    #     "renamed_columns": vancouver_renamed_columns,
+    #     "date_formats": ["%Y-%m-%d %H:%M", "%m/%d/%Y %H:%M"],
+    #     "read_csv_options": {
+    #         "encoding": "utf8-lossy",
+    #         "dtypes": {"Covered distance (m)": pl.Float64},
+    #     },
+    #     "processing_pipeline": [
+    #         "filter_null_rows",
+    #         "rename_columns",
+    #         "randomize_datetimes",
+    #         "offset_two_digit_years",
+    #         "select_final_columns",
+    #     ],
+    # },
 }
 
 # def format_files(files):
